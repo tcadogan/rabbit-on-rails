a76f0321288a9185ae02d4979f60f3937bf4af46
A Cadogan <tonycadogan@kbi-europe.com>
2013-01-25 10:57:15 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RabbitOnRails::Application.config.secret_token = '9004fda492332f28b11f58dc3b0f95f5630def4f5d02592b06334365eb1eb571f7c502fed4e0d1afae4a64962e7f3dbbb69a396e0aed75efb03cf10bc9cd980a'
